ad1048fe9d1940222686c164fdc6fe6338ba3816
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-08-03 07:52:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 bot_token = "1909102195:AAGeKwinn5v-UqNClO_TjZNciWsu82_H5ZA"
 sudo_chat_id = -1001457893155
 
-ARQ_API_KEY = "ZZRVOH-KAUHWO-ICMAYQ-QWTDZT-ARQ.
+ARQ_API_KEY = "ZZRVOH-KAUHWO-ICMAYQ-QWTDZT-ARQ"
 DeezerQuality = 3 # 1 = 128k Audio Quality
                   # 3 = 320k Audio Quality
                   # 9 = FLAC Format Very High Quality !
